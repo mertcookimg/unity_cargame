1190ea35a400cc91a7a1528205040a1cd8294919
Masato Kobayashi <mertcooking@gmail.com>
2024-05-08 00:26:37 +0900

    add: key input

--- car_game/Assets/Scripts/PlayerController.cs
+++ car_game/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
+    private float speed = 10.0f;
+    private float turnSpeed = 100.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
          // Move the vehicle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotate the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
         
     }
 }
